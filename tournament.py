f8f5844e4d2f973283690813ea2f471ef7391237
harish <harish.rana@staqu.com>
2016-12-19 23:19:53 +0530

    Added player deletion and updation

--- tournament.py
+++ tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -8,27 +8,62 @@ import psycopg2
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+    try:
+        db =  psycopg2.connect("dbname=tournament")
+        print "connected to database" , db
+        return db
+    except Exception as e:
+        print "what the hell!!!"
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    connection = connect()
+    c  = connection.cursor();
+    query = "DELETE FROM matches;"
+    c.execute(query);
+    connection.commit();
+    connection.close
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    connection = connect()
+    c  = connection.cursor();
+    query = "DELETE FROM players;"
+    c.execute(query);
+    connection.commit();
+    connection.close
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    connection = connect()
+    c  = connection.cursor();
+    query = "SELECT count(*) from players;"
+    c.execute(query);
+    result = c.fetchall();
+    connection.commit;
+    connection.close
+    print "result is ",result
+    print "result[0][0] is --> " , result[0][0]
+    return result[0][0]
 
 
 def registerPlayer(name):
+    """Returns the number of players currently registered."""
+    connection = connect()
+    c  = connection.cursor();
+    query = "INSERT INTO players (name) VALUES (%s);"
+    print query
+    c.execute(query,(name,));
+    connection.commit();
+    connection.close;
+
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -56,16 +91,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +108,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
--- tournament.sql
+++ tournament.sql
@@ -4,7 +4,7 @@
 -- statements if you choose to use it.
 CREATE TABlE players (player_id SERIAL UNIQUE NOT NULL,
                       name TEXT );
-CREATE TABLE matches (match_id SERIAL UNIQUE NOT NULL,player_1_id int, player_2_id int, winner int,loser int);
+CREATE TABLE matches (match_id SERIAL UNIQUE NOT NULL, winner_id int,loser_id int);
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 CREATE TABLE standings (standings_id int REFERENCES players(player_id), points integer);
